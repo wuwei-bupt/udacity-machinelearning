a68e51b327c9412acb6ef79d16fb19851d8d88f3
wuwei-bupt <75452600@qq.com>
2016-10-12 11:07:29 +0800

    multiply and divide the score to see what happens

--- deep-learning/softmax.py
+++ deep-learning/softmax.py
@@ -7,33 +7,21 @@ import math
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     # TODO: Compute and return softmax(x)
-    if len(x.shape) == 1:
-        row, column = x.shape[0], 1
-        x = x.reshape((row, column))
-    else:
-        row, column = x.shape
-    result = np.array([[0.0] * column for i in range(row)])
+    return np.exp(x)/np.sum(np.exp(x), axis=0)
 
-    for j in range(column):
-        denominator = 0
-        for i in range(row):
-            denominator += math.exp(x[i][j])
-        for i in range(row):
-            result[i][j] = math.exp(x[i][j]) / denominator
-    if column == 1:
-        result = result.reshape((row,))
-    return result
-
-
+# test case 1
 scores = np.array([3.0, 1.0, 0.2])
 print(softmax(scores))
+# Multiply the score by 10. What happens?
+print(softmax(scores*10))
+# Divide the score by 10. What happens?
+print(softmax(scores/10))
 
 # Plot softmax curves
 import matplotlib.pyplot as plt
 
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
-print scores
 
 plt.plot(x, softmax(scores).T, linewidth=2)
 plt.show()
